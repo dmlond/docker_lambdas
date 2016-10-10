01cdf812e87623c3fccc10d67fd98a0c1ebde1b9
Darin London <darin.london@duke.edu>
2016-10-10 17:24:37 -0400

    added a /cat endpoint which passes the request.body to the docker container using STDIN uses the cat endpoint built with docker build -t cat rest/cat

--- rest/app.rb
+++ rest/app.rb
@@ -12,3 +12,11 @@ post '/echo' do
   container.remove
   resp
 end
+
+post '/cat' do
+  request.body.rewind
+  container = Docker::Container.create('Image' => 'cat', 'OpenStdin' => true, 'StdinOnce' => true)
+  resp = container.tap(&:start).attach(stdin: StringIO.new("#{request.body.read}\n"))[0][0]
+  container.remove
+  resp
+end
