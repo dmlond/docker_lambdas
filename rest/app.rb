f71c79da5b55d27213ad7c1f71beaa93ff74377f
Darin London <darin.london@duke.edu>
2016-10-11 09:56:23 -0400

    collects both response and errors, if presence

--- rest/app.rb
+++ rest/app.rb
@@ -8,15 +8,17 @@ end
 post '/echo' do
   request.body.rewind
   container = Docker::Container.create('Image' => 'echo', Cmd: [request.body.read])
-  resp = container.tap(&:start).attach(logs: true)[0][0]
+  resp, error_messages = container.tap(&:start).attach(logs: true)
+  $stderr.puts "GOT ERRORS #{error_messages.join("\n")}"
   container.remove
-  resp
+  resp.join('')
 end
 
 post '/cat' do
   request.body.rewind
   container = Docker::Container.create('Image' => 'cat', 'OpenStdin' => true, 'StdinOnce' => true)
-  resp = container.tap(&:start).attach(stdin: StringIO.new("#{request.body.read}\n"))[0][0]
+  resp, error_messages = container.tap(&:start).attach(stdin: StringIO.new("#{request.body.read}\n"))
+  $stderr.puts "GOT ERRORS #{error_messages.join("\n")}"
   container.remove
-  resp
+  resp.join('')
 end
